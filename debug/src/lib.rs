915617c6fe8f31ece3ead3af5c59ada7538f8dd1
Yao Fu <fubupc@gmail.com>
2023-08-20 22:52:07 +0800

    derive(CustomDebug): 05-phantom-data

--- debug/src/lib.rs
+++ debug/src/lib.rs
@@ -2,7 +2,8 @@ use proc_macro::TokenStream;
 use quote::quote;
 use syn::{
     parse_macro_input, parse_quote, spanned::Spanned, Attribute, Data, DataStruct, DeriveInput,
-    Error, Fields, FieldsNamed, GenericParam, Lit, LitStr, Meta, MetaNameValue, Result,
+    Error, Fields, FieldsNamed, GenericArgument, GenericParam, Lit, LitStr, Meta, MetaNameValue,
+    PathArguments, Result, ReturnType, Type, TypeParam,
 };
 
 #[proc_macro_derive(CustomDebug, attributes(debug))]
@@ -38,8 +39,11 @@ fn impl_derive(ast: &DeriveInput) -> Result<proc_macro2::TokenStream> {
 
     let mut generics = ast.generics.clone();
     for p in &mut generics.params {
-        if let GenericParam::Type(ty) = p {
-            ty.bounds.push(parse_quote!(::std::fmt::Debug));
+        if let GenericParam::Type(tp) = p {
+            let need_debug = fields.iter().any(|f| appear_out_of_phantom(tp, &f.ty));
+            if need_debug {
+                tp.bounds.push(parse_quote!(::std::fmt::Debug));
+            }
         }
     }
     let (impl_generics, ty_generics, where_clause) = generics.split_for_impl();
@@ -71,5 +75,55 @@ fn parse_debug_attr(attrs: &[Attribute]) -> Result<Option<LitStr>> {
             bad => Err(Error::new(bad.span(), r#"expected `#[debug = "..."]`"#)),
         },
         [_, dup, ..] => Err(Error::new(dup.span(), "duplicated `#[debug]`")),
+    }
+}
+
+fn appear_out_of_phantom(tp: &TypeParam, ty: &Type) -> bool {
+    match ty {
+        Type::Array(a) => appear_out_of_phantom(tp, a.elem.as_ref()),
+        Type::BareFn(_) => false,
+        Type::Group(g) => appear_out_of_phantom(tp, &g.elem),
+        Type::ImplTrait(_) => false,
+        Type::Infer(_) => false,
+        // FIXME: Macro in type position?
+        Type::Macro(_) => todo!(),
+        Type::Never(_) => false,
+        Type::Paren(p) => appear_out_of_phantom(tp, &p.elem),
+        Type::Path(p) => {
+            let Some(last) = p.path.segments.last() else {
+                return false;
+            };
+            if tp.ident == last.ident {
+                return true;
+            }
+            if last.ident == "PhantomData" {
+                return false;
+            }
+            match &last.arguments {
+                PathArguments::AngleBracketed(args) => {
+                    args.args.iter().any(|g| match g {
+                        GenericArgument::Type(t) => appear_out_of_phantom(tp, t),
+                        // FIXME: `T` appears in binding may not require `T: Debug`
+                        GenericArgument::Binding(_) => todo!(),
+                        _ => false,
+                    })
+                }
+                PathArguments::None => return false,
+                PathArguments::Parenthesized(args) => {
+                    let out = match &args.output {
+                        ReturnType::Default => false,
+                        ReturnType::Type(_, o) => appear_out_of_phantom(tp, o),
+                    };
+                    out || args.inputs.iter().any(|i| appear_out_of_phantom(tp, i))
+                }
+            }
+        }
+        Type::Ptr(_) => false,
+        Type::Reference(r) => appear_out_of_phantom(tp, &r.elem),
+        Type::Slice(s) => appear_out_of_phantom(tp, &s.elem),
+        Type::TraitObject(_) => false,
+        Type::Tuple(tup) => tup.elems.iter().any(|e| appear_out_of_phantom(tp, e)),
+        Type::Verbatim(_) => todo!(),
+        _ => todo!(),
     }
 }
