eac29459453e3b543c07517df0350b06d679c70e
Yao Fu <fubupc@gmail.com>
2023-09-08 17:16:03 +0800

    seq!: 04-paste-ident

--- seq/src/lib.rs
+++ seq/src/lib.rs
@@ -1,10 +1,12 @@
 use proc_macro::TokenStream;
 use proc_macro2::{Group, Literal, TokenStream as TokenStream2, TokenTree};
-use quote::ToTokens;
+use quote::{format_ident, TokenStreamExt};
 use syn::{
     braced,
     parse::{Parse, ParseStream},
-    parse_macro_input, token, Ident, LitInt, Result, Token,
+    parse_macro_input,
+    spanned::Spanned,
+    token, Error, Ident, LitInt, Result, Token,
 };
 
 #[proc_macro]
@@ -19,37 +21,91 @@ pub fn seq(input: TokenStream) -> TokenStream {
 fn impl_seq(seq: &Seq) -> Result<TokenStream2> {
     let start: usize = seq.start.base10_parse()?;
     let end: usize = seq.end.base10_parse()?;
-    let repeated: TokenStream2 = (start..end)
-        .map(|n| replace_ident(seq.content.clone(), &seq.ident, n))
-        .collect();
 
-    Ok(repeated)
-}
-
-fn replace_ident(content: TokenStream2, ident: &Ident, n: usize) -> TokenStream2 {
-    content
-        .into_iter()
-        .map(|tt| replace_in_tt(tt, ident, n))
+    (start..end)
+        .map(|n| process_ident(seq.content.clone(), &seq.ident, n))
         .collect()
 }
 
-fn replace_in_tt(tt: TokenTree, ident: &Ident, n: usize) -> TokenTree {
-    match tt {
-        TokenTree::Group(g) => {
-            let delimiter = g.delimiter();
-            let replaced = replace_ident(g.stream(), ident, n);
-            TokenTree::Group(Group::new(delimiter, replaced))
-        }
-        TokenTree::Ident(i) => {
-            if &i == ident {
-                TokenTree::Literal(Literal::usize_unsuffixed(n))
-            } else {
-                TokenTree::Ident(i)
+// fn replace_ident(stream: TokenStream2, ident: &Ident, n: usize) -> TokenStream2 {
+//     stream
+//         .into_iter()
+//         .map(|tt| match tt {
+//             TokenTree::Group(g) => {
+//                 let delimiter = g.delimiter();
+//                 let replaced = replace_ident(g.stream(), ident, n);
+//                 TokenTree::Group(Group::new(delimiter, replaced))
+//             }
+//             TokenTree::Ident(i) if &i == ident => TokenTree::Literal(Literal::usize_unsuffixed(n)),
+//             other => other,
+//         })
+//         .collect()
+// }
+
+fn process_ident(stream: TokenStream2, seq_ident: &Ident, num: usize) -> Result<TokenStream2> {
+    let mut stream = stream.into_iter().peekable();
+    let mut result = TokenStream2::new();
+
+    while let Some(tt) = stream.next() {
+        match tt {
+            TokenTree::Group(g) => {
+                let delimiter = g.delimiter();
+                let pasted = process_ident(g.stream(), seq_ident, num)?;
+                result.append(TokenTree::Group(Group::new(delimiter, pasted)))
             }
+            TokenTree::Ident(i) => {
+                if &i == seq_ident {
+                    match stream.peek() {
+                        Some(TokenTree::Punct(p)) if p.as_char() == '~' => return Err(Error::new(
+                            i.span(),
+                            "identifier cannot be start with number (produced by seq identifier)",
+                        )),
+                        _ => result.append(TokenTree::Literal(Literal::usize_unsuffixed(num))),
+                    };
+                } else {
+                    let mut curr = i;
+                    loop {
+                        match stream.peek() {
+                            Some(TokenTree::Punct(p)) if p.as_char() == '~' => {
+                                stream.next(); // pop ~
+                                match stream.next() {
+                                    Some(TokenTree::Ident(i2)) => {
+                                        if &i2 == seq_ident {
+                                            curr = format_ident!("{}{}", curr, num);
+                                        } else {
+                                            curr = format_ident!("{}{}", curr, i2);
+                                        }
+                                    }
+                                    other => {
+                                        return Err(Error::new(
+                                            other.span(),
+                                            "paste operator ~ must be followed by an identifier",
+                                        ))
+                                    }
+                                }
+                            }
+                            _ => {
+                                result.append(curr);
+                                break;
+                            }
+                        };
+                    }
+                }
+            }
+            TokenTree::Punct(p) => {
+                if p.as_char() == '~' {
+                    return Err(Error::new(
+                        p.span(),
+                        "paste operator ~ must be preceded by an identifier",
+                    ));
+                } else {
+                    result.append(TokenTree::Punct(p))
+                }
+            }
+            TokenTree::Literal(l) => result.append(TokenTree::Literal(l)),
         }
-        TokenTree::Punct(p) => TokenTree::Punct(p),
-        TokenTree::Literal(l) => TokenTree::Literal(l),
     }
+    Ok(result)
 }
 
 struct Seq {
